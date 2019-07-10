95159155f69e6efaeaa433c31478c0a6a8477bba
Rémy Trohel <remy.trohel@gmail.com>
2019-07-10 16:04:53 +0200

    init maze size 7

--- src/main/java/com/wildcodeschool/BurgerRun/Repositories/MazeRepository.java
+++ src/main/java/com/wildcodeschool/BurgerRun/Repositories/MazeRepository.java
@@ -3,13 +3,29 @@ package com.wildcodeschool.BurgerRun.Repositories;
 import com.wildcodeschool.BurgerRun.Entities.Maze;
 
 public class MazeRepository {
-    private static Maze maze;
+    private static Maze maze = getMaze();
 
-    public Maze getMaze() {
+    public static Maze getMaze() {
         if (maze == null) {
             maze = new Maze();
         }
         return maze;
     }
 
+    public void initMaze() {
+        int size = maze.getSize();
+        String[] strings = {
+            "#######",
+            "#   # S",
+            "# # # #",
+            "# # # #",
+            "E #   #",
+            "#######"
+        };
+        String[][] matrice = new String[size][];
+        for (int i = 0; i < size; i++) {
+            matrice[i] = strings[i].split("");
+        }
+        maze.setMatrice(matrice);
+    }
 }
