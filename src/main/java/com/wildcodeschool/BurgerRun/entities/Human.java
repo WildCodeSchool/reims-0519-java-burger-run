c585f7944498c29a829433a3b09476c2ad53d502
Julien Risacher <julien.risacher@gmail.com>
2019-07-11 14:53:54 +0200

    change Human and Burger

--- src/main/java/com/wildcodeschool/BurgerRun/entities/Human.java
+++ src/main/java/com/wildcodeschool/BurgerRun/entities/Human.java
@@ -1,38 +1,17 @@
 package com.wildcodeschool.BurgerRun.entities;
 
 public class Human {
-    private String human_name;
-    private int xHuman;
-    private int yHuman;
+    private int idHuman;
 
-    public String getHuman_name() {
-        return human_name;
+    public int getIdHuman() {
+        return idHuman;
     }
 
-    public void setHuman_name(String human_name) {
-        this.human_name = human_name;
+    public void setIdHuman(int idHuman) {
+        this.idHuman = idHuman;
     }
 
-    public int getxHuman() {
-        return xHuman;
-    }
-
-    public void setxHuman(int xHuman) {
-        this.xHuman = xHuman;
-    }
-
-    public int getyHuman() {
-        return yHuman;
-    }
-
-    public void setyHuman(int yHuman) {
-        this.yHuman = yHuman;
-    }
-
-    public Human(String human_name, int xHuman, int yHuman) {
-        this.human_name = human_name;
-        this.xHuman = xHuman;
-        this.yHuman = yHuman;
-    }
-    
+    public Human(int idHuman) {
+        this.idHuman = idHuman;
+    }    
 }
