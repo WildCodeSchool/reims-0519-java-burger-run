e6b51096cc14df838330a271647c36afddd58ce9
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-11 14:00:47 +0200

    add import

--- src/main/java/com/wildcodeschool/BurgerRun/controllers/PageController.java
+++ src/main/java/com/wildcodeschool/BurgerRun/controllers/PageController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 class PageController {
